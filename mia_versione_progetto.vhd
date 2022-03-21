34bb088dfb12e8a6dee35f3d02ccf33f026be67f
lups2000 <100372313+lups2000@users.noreply.github.com>
2022-03-21 16:56:48 +0100

    Update mia_versione_progetto.vhd
    Versione Finale!Funziona tutto!Magari da sistemare un pò esteticamente

--- mia_versione_progetto.vhd
+++ mia_versione_progetto.vhd
@@ -52,7 +52,9 @@ architecture Behavioral of project_reti_logiche is
     signal last_byte_address: std_logic_vector(15 downto 0 ) := (others => '0');    --indirizzo ultimo byte letto
     signal current_byte_address: std_logic_vector(15 downto 0 ) := (others => '0'); --indirizzo byte corrente
     --signal current_byte: unsigned(7 downto 0):= (others => '0');                 --byte corrente
-    signal stato_conv: std_logic_vector(1 downto 0) := (others => '0');              --stato convolutore
+    signal stato_conv: std_logic_vector(1 downto 0) := "00";              --stato convolutore
+    signal offset_999: std_logic_vector(15 downto 0):= "0000001111100111";
+
 
     procedure init_loop(
         signal o_address : out std_logic_vector(15 downto 0);
@@ -71,15 +73,18 @@ begin
     --variabile per memorizzare l'uscita
     variable uscita: std_logic_vector(0 to 15) := (others => '0');
     --contatore
-    variable count : integer := 7;
-    variable i : integer := 0;
-    variable j : integer := 0;
+    variable count : integer := 7; --per scorrere tutti i bit del byte che leggo
+    variable i : integer := 0; --per scrivere tutti i bit nel vettore uscita
+    variable j : integer := 0; --per gestire la scrittura dei due byte in memoria
+    variable z : integer := 0; --per aiutarmi a scrivere i due byte al giusto indirizzo
+
 
     begin
         if rising_edge(i_clk) then
             o_done <= '0';
             o_en <= '0';
             o_we <= '0';
+            --stato_conv <= "00";
             o_data    <= (others => '0');
             o_address <= (others => '0');
             if i_rst = '1' then
@@ -131,10 +136,10 @@ begin
                         --calcolo nuovo indirizzo
                         var := unsigned(current_byte_address)+1;
                         --controllo se tutti i byte sono stati letti
-                        if not(std_logic_vector(var)=last_byte_address) then --se non sono ancora stati letti tutti
+                        if not(std_logic_vector(var)=last_byte_address +1) then --se non sono ancora stati letti tutti
                             --aggiorno gli indirizzi
                             current_byte_address <= std_logic_vector(var);
-                            o_address <= std_logic_vector(var + 1);
+                            o_address <= std_logic_vector(var);
                             --attivo segnale per la lettura da memoria
                             o_en <= '1';
                             has_byte <=true;
@@ -222,16 +227,23 @@ begin
                             -- attivo segnale di scrittura
                             o_we <= '1';
                             if(j=0) then -- per scrivere il primo byte del vettore uscita
+
                                 --devo settare l'indirizzo di scrittura
+                                --non va bene cosi
+                                o_address <= std_logic_vector(unsigned(current_byte_address) + unsigned(offset_999)+z);
                                 o_data <= uscita(0 to 7);
                                 j := j+1;
                                 state <= SCRITTURA_BYTE;
                             else -- per scrivere il secondo byte del vettore uscita
+
                                 --devo settare l'indirizzo di scrittura
+                                --non va bene cosi
+                                o_address <= std_logic_vector(unsigned(current_byte_address) + unsigned(offset_999)+z+1);
                                 o_data <= uscita(8 to 15);
                                 j := 0;
                                 i := 0;
                                 count :=7;
+                                z := z+1;
                                 state <= ATTESA_LETTURA_BYTE;
                             end if;    
 
@@ -239,6 +251,8 @@ begin
 
                     when FINE =>
                         if (i_start = '0') then
+                            z :=0;
+                            stato_conv <= "00";
                             state <= INIZIO;
                         else 
                             o_done <= '1';
